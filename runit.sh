ccaec5dbf62c43001fff6c6fe76c62c822abeafc
Autocrit <tom@Toms-Mac-Pro.home>
2015-07-20 21:55:42 +0100

    Day

--- runit.sh
+++ runit.sh
@@ -1,9 +1,14 @@
+day=4
+day="Day$(printf '%03d' $day)"
+
 #/Users/tom/GitHub/KitchenSync/KitchenSync.sh \
 #	--auto-folder-naming "/Day003_$(date +'%Y%m%d')/Offload" \
 #	/Volumes/EOS_DIGITAL/DCIM/100EOS5D \
 #	/Users/tom/Documents/Temp
 #	
+
 /Users/tom/GitHub/KitchenSync/KitchenSync.sh \
-	--auto-folder-naming "/Day003/Offload" \
+	--auto-folder-naming "/$day/Offload" \
 	/Volumes/EOS_DIGITAL/DCIM/100EOS5D \
 	/Users/tom/Documents/Temp
+
