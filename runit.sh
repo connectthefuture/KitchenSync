0a87a9840f405f6a3c915d213143491949a2eded
Autocrit <tom@Toms-Mac-Pro.home>
2015-07-20 21:25:21 +0100

    First commit

---
+++ runit.sh
@@ -0,0 +1,9 @@
+#/Users/tom/GitHub/KitchenSync/KitchenSync.sh \
+#	--auto-folder-naming "/Day003_$(date +'%Y%m%d')/Offload" \
+#	/Volumes/EOS_DIGITAL/DCIM/100EOS5D \
+#	/Users/tom/Documents/Temp
+#	
+/Users/tom/GitHub/KitchenSync/KitchenSync.sh \
+	--auto-folder-naming "/Day003/Offload" \
+	/Volumes/EOS_DIGITAL/DCIM/100EOS5D \
+	/Users/tom/Documents/Temp
